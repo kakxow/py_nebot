822c25f73120cb026fe3a05370afb8cecf435a3c
kakxow <kakxow@gmail.com>
2020-05-08 14:33:07 +0300

    Refactor: calling triggers in handler improved.

--- nechatbot/handler.py
+++ nechatbot/handler.py
@@ -3,28 +3,26 @@ from . import triggers
 
 
 async def on_message(bot, msg: dict):
-    _chat = msg.get('chat', {})
-    chat_id = _chat.get('id', '')
+    chat = msg.get('chat', {})
+    chat_id = chat.get('id', '')
 
     new_chat_member = msg.get('new_chat_member', '')
     if new_chat_member:
         message_id = msg.get('message_id', '')
-        await bot.send_sticker(
+        return await bot.send_sticker(
             chat_id, greeting_sticker,
             reply_to_message_id=message_id
         )
-        return
 
     text = msg.get('text', '')
     if not text:
         return
     if 'ето не чат' in text:
-        await bot.set_chat_title(chat_id, text)
-        return
-    trigger_list = (trigger for trigger in triggers.__dict__.values()
-                    if callable(trigger))
-    for trigger in trigger_list:
+        return await bot.set_chat_title(chat_id, text)
+
+    callable_reiggers = \
+        (getattr(triggers, trigger) for trigger in triggers.__all__)
+    for trigger in callable_reiggers:
         message = await trigger(text.lower())
         if message:
-            await bot.send_message(chat_id, message)
-            return
+            return await bot.send_message(chat_id, message)
--- nechatbot/triggers.py
+++ nechatbot/triggers.py
@@ -1,6 +1,32 @@
 import random
+import re
+from typing import Tuple
 
 from . import constants, get_dog
+
+__all__ = [
+    "ukraine",
+    "swearing",
+    "hate_speech",
+    "corgi",
+    "shibe",
+    "random_dog",
+    "toy",
+    "pug",
+    "terrier",
+    "trista",
+    "net",
+]
+
+
+def words_in_text(words: Tuple[str, ...], text: str) -> bool:
+    return any(word in text for word in words)
+
+
+def words_in_text_re(words: Tuple[str, ...], text: str) -> bool:
+    pattern = r'\b' + r'\b|\b'.join(words) + r'\b'
+    match = re.search(pattern, text, re.I)
+    return bool(match)
 
 
 async def ukraine(text: str) -> str:
@@ -8,49 +34,13 @@ async def ukraine(text: str) -> str:
 
 
 async def swearing(text: str) -> str:
-    is_swearing = any(word in text for word in constants.trash)
+    is_swearing = words_in_text(constants.trash, text)
     return "а давайте не материться" if is_swearing else ""
 
 
 async def hate_speech(text: str) -> str:
-    is_hate_speech = any(word in text for word in constants.hate_speech)
+    is_hate_speech = words_in_text(constants.hate_speech, text)
     return "это хейтспич приятель" if is_hate_speech else ""
-
-
-async def corgi(text: str) -> str:
-    corgi_url = "https://dog.ceo/api/breed/corgi/cardigan/images/random"
-    has_corgi = any(word in text for word in constants.corgi)
-    return await get_dog.get(corgi_url) if has_corgi else ""
-
-
-async def shibe(text: str) -> str:
-    shibe_url = "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false"
-    has_shibe = any(word in text for word in constants.shibe)
-    return await get_dog.get(shibe_url) if has_shibe else ""
-
-
-async def random_dog(text: str) -> str:
-    random_dog_url = "https://dog.ceo/api/breeds/image/random"
-    has_random_dog = any(word in text for word in constants.random_dog)
-    return await get_dog.get(random_dog_url) if has_random_dog else ""
-
-
-async def toy(text: str) -> str:
-    toy_url = "https://dog.ceo/api/breed/terrier/toy/images/random"
-    has_toy = any(word in text for word in constants.toy)
-    return await get_dog.get(toy_url) if has_toy else ""
-
-
-async def pug(text: str) -> str:
-    pug_url = "https://dog.ceo/api/breed/pug/images/random"
-    has_pug = any(word in text for word in constants.pug)
-    return await get_dog.get(pug_url) if has_pug else ""
-
-
-async def terrier(text: str) -> str:
-    terrier_url = "https://dog.ceo/api/breed/terrier/images/random"
-    has_terrier = any(word in text for word in constants.terrier)
-    return await get_dog.get(terrier_url) if has_terrier else ""
 
 
 async def trista(text: str) -> str:
@@ -59,3 +49,39 @@ async def trista(text: str) -> str:
 
 async def net(text: str) -> str:
     return random.choice(constants.net) if text.endswith("нет") else ""
+
+
+async def random_dog(text: str) -> str:
+    random_dog_url = "https://dog.ceo/api/breeds/image/random"
+    has_random_dog = words_in_text(constants.random_dog, text)
+    return await get_dog.get(random_dog_url) if has_random_dog else ""
+
+
+async def corgi(text: str) -> str:
+    corgi_url = "https://dog.ceo/api/breed/corgi/images/random"
+    has_corgi = words_in_text(constants.corgi, text)
+    return await get_dog.get(corgi_url) if has_corgi else ""
+
+
+async def shibe(text: str) -> str:
+    shibe_url = "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false"
+    has_shibe = words_in_text(constants.shibe, text)
+    return await get_dog.get(shibe_url) if has_shibe else ""
+
+
+async def toy(text: str) -> str:
+    toy_url = "https://dog.ceo/api/breed/terrier/toy/images/random"
+    has_toy = words_in_text(constants.toy, text)
+    return await get_dog.get(toy_url) if has_toy else ""
+
+
+async def pug(text: str) -> str:
+    pug_url = "https://dog.ceo/api/breed/pug/images/random"
+    has_pug = words_in_text(constants.pug, text)
+    return await get_dog.get(pug_url) if has_pug else ""
+
+
+async def terrier(text: str) -> str:
+    terrier_url = "https://dog.ceo/api/breed/terrier/images/random"
+    has_terrier = words_in_text(constants.terrier, text)
+    return await get_dog.get(terrier_url) if has_terrier else ""
