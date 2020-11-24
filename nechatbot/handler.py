d96617188753837f1726395694b47401d2399157
Egor M <ave2@me.com>
2020-11-24 22:02:36 +0300

    Improvements/rework triggers (#2)
    * :recycle: introduce predicates & improve triggers structure and logic
    * :bug: fix title change & introduce black formatting
    * :white_check_mark: fix trista mid text test

--- nechatbot/handler.py
+++ nechatbot/handler.py
@@ -1,27 +1,27 @@
-from .constants import greeting_sticker
 from . import triggers
+from .constants import greeting_sticker, change_title_prefixes
+from .predicates import is_message_startswith
 
 
 async def on_message(bot, msg: dict):
-    chat = msg.get('chat', {})
-    chat_id = chat.get('id', '')
+    chat = msg.get("chat", {})
+    chat_id = chat.get("id", "")
 
-    new_chat_member = msg.get('new_chat_member', '')
+    new_chat_member = msg.get("new_chat_member", "")
     if new_chat_member:
-        message_id = msg.get('message_id', '')
+        message_id = msg.get("message_id", "")
         return await bot.send_sticker(
-            chat_id, greeting_sticker,
-            reply_to_message_id=message_id
+            chat_id, greeting_sticker, reply_to_message_id=message_id
         )
 
-    text = msg.get('text', '')
+    text = msg.get("text", "")
     if not text:
         return
-    if 'ето не чат' in text.lower():
+
+    if is_message_startswith(text, *change_title_prefixes):
         return await bot.set_chat_title(chat_id, text)
 
-    callable_triggers = \
-        (getattr(triggers, trigger) for trigger in triggers.__all__)
+    callable_triggers = (getattr(triggers, trigger) for trigger in triggers.__all__)
     for trigger in callable_triggers:
         message = await trigger(text.lower())
         if message:
---
+++ nechatbot/predicates.py
@@ -0,0 +1,44 @@
+import re
+from typing import Set
+
+WORDS_PATTERN = re.compile(r"\w+")
+FIRST_WORD_PATTERN = re.compile(r"^\w+")
+LAST_WORD_PATTERN = re.compile(r"\w+$")
+
+
+def extract_words(message: str) -> Set[str]:
+    words = WORDS_PATTERN.findall(message.lower())
+    return set(words)
+
+
+def is_message_contains_words(message: str, *words_to_find: str) -> bool:
+    message_words = extract_words(message)
+    return any(word.lower() in message_words for word in words_to_find)
+
+
+def is_message_contains_phrases(message: str, *phrases: str) -> bool:
+    return any(phrase.lower() in message.lower() for phrase in phrases)
+
+
+def is_message_startswith(message: str, *prefixes: str) -> bool:
+    return any(message.lower().startswith(prefix.lower()) for prefix in prefixes)
+
+
+def is_message_starts_with_word(message: str, *word_prefixes: str) -> bool:
+    match = FIRST_WORD_PATTERN.search(message.lower())
+    if match is None:
+        return False
+    first_word = match.group()
+    return any(first_word == word.lower() for word in word_prefixes)
+
+
+def is_message_endswith(message: str, *postfixes: str) -> bool:
+    return any(message.lower().endswith(postfix.lower()) for postfix in postfixes)
+
+
+def is_message_ends_with_word(message: str, *word_postfixes: str) -> bool:
+    match = LAST_WORD_PATTERN.search(message.lower())
+    if match is None:
+        return False
+    last_word = match.group()
+    return any(last_word == postfix.lower() for postfix in word_postfixes)
