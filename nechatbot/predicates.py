53a2f214736b08d41d4f756da669ef0114d059d1
kakxow <48805736+kakxow@users.noreply.github.com>
2021-04-22 14:49:10 +0300

    Emojis (#8)
    * Emoji support in predicates.
    * Now reacts to dog emojis.
    
    Co-authored-by: Maksim Sukhanovm <kakxow@gmail.com>

--- nechatbot/predicates.py
+++ nechatbot/predicates.py
@@ -8,6 +8,31 @@ from .calendar import day_month_from_date
 WORDS_PATTERN = re.compile(r"\w+")
 FIRST_WORD_PATTERN = re.compile(r"^\w+")
 LAST_WORD_PATTERN = re.compile(r"\w+$")
+WORDS_AND_EMOJIS_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    r"]|\w+"
+)  # https://gist.github.com/Alex-Just/e86110836f3f93fe7932290526529cd1#gistcomment-3236190
+
+
+def extract_words_and_emojis(message: str) -> Set[str]:
+    words_and_emojis = WORDS_AND_EMOJIS_PATTERN.findall(message.lower())
+    return set(words_and_emojis)
+
+
+def is_message_contains_words_and_emojis(message: str, *words_to_find: str) -> bool:
+    message_words = extract_words_and_emojis(message)
+    return any(word.lower() in message_words for word in words_to_find)
 
 
 def extract_words(message: str) -> Set[str]:
