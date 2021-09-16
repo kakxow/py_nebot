948fc63ea26d4122e6b150f5888e50509b96c47a
Maksim Sukhanovm <kakxow@gmail.com>
2021-09-16 17:49:04 +0300

    Fix: text is a response attribute, not a method.

--- nechatbot/get_frog.py
+++ nechatbot/get_frog.py
@@ -9,7 +9,7 @@ frog_img_pattern = r"<img class=\"lazy thumbnail aspect-wide-contain\" .* data-s
 async def get(url: str) -> str:
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
-        text = response.text()
+        text = response.text
     match = re.search(frog_img_pattern, text, re.M)
     if match:
         return match.group(1)
