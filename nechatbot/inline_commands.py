e6596b78fa8c191ea995f1b577b722c4202c9aae
Maksim Sukhanovm <kakxow@gmail.com>
2021-11-03 15:34:03 +0300

    Added button in birthday reply

--- nechatbot/inline_commands.py
+++ nechatbot/inline_commands.py
@@ -11,11 +11,22 @@ async def add_birthday(inline_query) -> list:
             text_message_content = {
                 "message_text": f"Created {user['first_name']}'s card with birthday date {date}"
             }
+            inline_keyboard_markup = {
+                "inline_keyboard": [
+                    [
+                        {
+                            "text": "Add your birthday too!",
+                            "switch_inline_query_current_chat": "",
+                        }
+                    ]
+                ]
+            }
             inline_query_result = {
                 "type": "article",
                 "id": "1",
-                "title": "Add birthday date",
+                "title": "Add birthday",
                 "input_message_content": text_message_content,
+                "reply_markup": inline_keyboard_markup,
             }
             return [inline_query_result]
     return []
