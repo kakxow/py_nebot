b46cc623d610ed15276782c384f082c5d17d10b3
Maksim Sukhanovm <kakxow@gmail.com>
2022-03-09 15:41:18 +0300

    New locations - baku, ist, tbl, yer

--- nechatbot/inline_commands.py
+++ nechatbot/inline_commands.py
@@ -1,9 +1,6 @@
 from .predicates import is_date
 
-__all__ = [
-    "add_birthday",
-    "choose_city"
-]
+__all__ = ["add_birthday", "choose_city"]
 
 
 async def add_birthday(inline_query) -> list:
@@ -38,15 +35,13 @@ async def add_birthday(inline_query) -> list:
 async def choose_city(inline_query):
     text = inline_query["query"]
     if not text:
-        text_message_content_msk = {
-            "message_text": "I'm in Moscow now!"
-        }
-        text_message_content_spb = {
-            "message_text": "I'm in St. Petersburg now!"
-        }
-        text_message_content_remove = {
-            "message_text": "I'm in undefined now!"
-        }
+        text_message_content_msk = {"message_text": "I'm in Moscow now!"}
+        text_message_content_spb = {"message_text": "I'm in St. Petersburg now!"}
+        text_message_content_baku = {"message_text": "I'm in Baku now!"}
+        text_message_content_ist = {"message_text": "I'm in Istanbul now!"}
+        text_message_content_tbl = {"message_text": "I'm in Tbilisi now!"}
+        text_message_content_yer = {"message_text": "I'm in Yereven now!"}
+        text_message_content_remove = {"message_text": "I'm in undefined now!"}
         inline_keyboard_markup = {
             "inline_keyboard": [
                 [
@@ -57,25 +52,58 @@ async def choose_city(inline_query):
                 ]
             ]
         }
-        inline_query_result_msk = {
+        inline_query_result = {
             "type": "article",
+            "reply_markup": inline_keyboard_markup,
+        }
+        inline_query_result_msk = {
+            **inline_query_result,
             "id": "msk",
             "title": "msk",
             "input_message_content": text_message_content_msk,
-            "reply_markup": inline_keyboard_markup,
         }
         inline_query_result_spb = {
-            "type": "article",
+            **inline_query_result,
             "id": "spb",
             "title": "spb",
             "input_message_content": text_message_content_spb,
-            "reply_markup": inline_keyboard_markup,
+        }
+        inline_query_result_baku = {
+            **inline_query_result,
+            "id": "baku",
+            "title": "baku",
+            "input_message_content": text_message_content_baku,
+        }
+        inline_query_result_ist = {
+            **inline_query_result,
+            "id": "ist",
+            "title": "ist",
+            "input_message_content": text_message_content_ist,
+        }
+        inline_query_result_tbl = {
+            **inline_query_result,
+            "id": "tbl",
+            "title": "tbl",
+            "input_message_content": text_message_content_tbl,
+        }
+        inline_query_result_yer = {
+            **inline_query_result,
+            "id": "yer",
+            "title": "yer",
+            "input_message_content": text_message_content_yer,
         }
         inline_query_result_remove = {
-            "type": "article",
+            **inline_query_result,
             "id": "remove",
             "title": "remove",
             "input_message_content": text_message_content_remove,
-            "reply_markup": inline_keyboard_markup,
         }
-        return [inline_query_result_msk, inline_query_result_spb, inline_query_result_remove]
+        return [
+            inline_query_result_msk,
+            inline_query_result_spb,
+            inline_query_result_baku,
+            inline_query_result_ist,
+            inline_query_result_tbl,
+            inline_query_result_yer,
+            inline_query_result_remove,
+        ]
