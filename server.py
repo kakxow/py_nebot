7bf10d5b61ce105dd6ad0976c610e1bd1f5c1198
Maksim Sukhanovm <kakxow@gmail.com>
2022-09-05 14:33:07 +0300

    asgi_tools

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
 import asyncio
 
-from flask import Flask, request, abort
+from asgi_tools import App
+
 
 from main import bot
 from nechatbot.constants import SECURITY_KEY
 
-
-app = Flask(__name__)
+app = App()
 
 
 @app.route("/", methods=["POST"])
-def get_update():
+def get_update(request):
+    print(request)
     if request.headers.get("X-Telegram-Bot-Api-Secret-Token", "") != SECURITY_KEY:
-        return abort(401)
+        return 401
     update = request.json
     asyncio.ensure_future(bot.process_update(update))
     return "OK", 200
+
+
+# run(app, host="localhost", port=80)
