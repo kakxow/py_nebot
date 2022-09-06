4f659c8c1c69b7528d443598469ac843274f35ca
kakxow <kakxow@gmail.com>
2022-09-06 22:18:11 +0300

    Corrected typing for server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from asgi_tools import App, ResponseError
+from asgi_tools import App, ResponseError, Request
 
 from main import bot
 from nechatbot.constants import SECURITY_KEY
@@ -9,7 +9,7 @@ app = App()
 
 
 @app.route("/", methods=["POST"])
-async def get_update(request):
+async def get_update(request: Request) -> tuple[int, str] | ResponseError:
     print(request)
     if request.headers.get("X-Telegram-Bot-Api-Secret-Token", "") != SECURITY_KEY:
         return ResponseError(status_code=401)
