f9a8a5994a0b9fe1fcad9daf01ccb0b7d0aa8691
kakxow <48805736+kakxow@users.noreply.github.com>
2022-11-05 10:58:50 +0300

    Frogs fixed.
    The path to a photo on the frog site was changed to a relative from absolute

--- nechatbot/get_frog.py
+++ nechatbot/get_frog.py
@@ -12,6 +12,6 @@ async def get(url: str) -> str:
         text = response.text
     match = re.search(frog_img_pattern, text, re.M)
     if match:
-        return match.group(1)
+        return f"https://www.generatormix.com{match.group(1)}"
     else:
         return ""
