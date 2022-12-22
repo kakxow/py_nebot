0799bc78ac618185dcfffbabe6b3264da6f62d30
kakxow <kakxow@gmail.com>
2022-12-22 19:57:23 +0300

    New models and relations for tags.

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from nechatbot.db import engine as nechatbot_db_engine
+from nechatbot.nechat_db_types import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- nechatbot/nechat_db_types.py
+++ nechatbot/nechat_db_types.py
@@ -1,8 +1,62 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Table,
+    ForeignKey,
+    ForeignKeyConstraint,
+    Identity,
+)
 
 
 Base = declarative_base()
+
+
+tags_for_users = Table(
+    "tags_for_users",
+    Base.metadata,
+    Column("user_id", Integer, primary_key=True),
+    Column("user_chat_id", Integer, primary_key=True),
+    Column("tag_id", ForeignKey("tags.id"), primary_key=True),
+    ForeignKeyConstraint(
+        ["user_chat_id", "user_id"],
+        ["users.chat", "users.id"],
+    ),
+)
+
+mentions_for_tags = Table(
+    "mentions_for_tags",
+    Base.metadata,
+    Column("tag_id", ForeignKey("tags.id"), primary_key=True),
+    Column("mention_id", ForeignKey("mention_tags.id"), primary_key=True),
+)
+
+
+class MentionTag(Base):
+    __tablename__ = "mention_tags"
+
+    id = Column(Integer, Identity(), primary_key=True)
+    name = Column(String(64), unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return "<MentionTag(name='%s')>" % (self.name,)
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id = Column(Integer, Identity(), primary_key=True)
+    name = Column(String(64), unique=True, nullable=False)
+    mentions = relationship(
+        MentionTag, secondary=mentions_for_tags, collection_class=list
+    )
+
+    def __repr__(self) -> str:
+        return "<Tag(name='%s', mentions=[%s])>" % (
+            self.name,
+            self.mentions,
+        )
 
 
 class User(Base):
@@ -16,6 +70,7 @@ class User(Base):
     birthday = Column(String(64))
     credit = Column(Integer)
     location = Column(String(64))
+    tags = relationship(Tag, secondary=tags_for_users, collection_class=list)
 
     def __repr__(self) -> str:
         return "<User(first_name='%s', last_name='%s', username='%s')>" % (
